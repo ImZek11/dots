b315a92d8c53462357333ccff9e3a3c43ee7abd2
Zek11 <ezequieldsromano@gmail.com>
2026-04-11 11:58:37 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+read -p "Start installation? (y/n): " choice
+choice=${choice,,}
+
+if [[ "$choice" == "y" ]]; then
+    mv ~/dots11/.config/* ~/.config/
+    mv ~/dots11/Pictures/ ~/
+    echo "Configs And Pictures installed successfully!"
+
+    # Second prompt
+    read -p "Do you want to continue with the installation? (y/n): " choice2
+    choice2=${choice2,,}
+    if [[ "$choice2" == "y" ]]; then
+        yay -S hyprland kitty sddm waybar fastfetch yazi swayn hyprshot hyprpaper hypridle hyprlock polkit-gnome nvim btop impala wiremix cava rmpc nwg-look mpd mpc walker peaclock oh-my-posh
+        echo "Enabling SDDM"
+        sudo systemctl enable sddm
+        echo "Programs installed and setup succesfully!"
+
+        # Third prompt
+        read -p "Do you want to proceed with theme installation? (y/n): " choice3
+        choice3=${choice3,,}
+        if [[ "$choice3" == "y" ]]; then
+            yay -S catppuccin-gtk-theme catppuccin-cursors-mocha
+            echo "Installation fully completed!"
+
+            # Fourth prompt: reboot
+            read -p "Do you want to reboot now? (y/n): " choice4
+            choice4=${choice4,,}
+            if [[ "$choice4" == "y" ]]; then
+                echo "Rebooting..."
+                sudo reboot
+            elif [[ "$choice4" == "n" ]]; then
+                echo "Reboot cancelled. You can reboot later if needed."
+            else
+                echo "Invalid input. Please enter 'y' or 'n'."
+            fi
+
+        elif [[ "$choice3" == "n" ]]; then
+            echo "Installation cancelled."
+        else
+            echo "Invalid input. Please enter 'y' or 'n'."
+        fi
+
+    elif [[ "$choice2" == "n" ]]; then
+        echo "Installation cancelled."
+    else
+        echo "Invalid input. Please enter 'y' or 'n'."
+    fi
+
+elif [[ "$choice" == "n" ]]; then
+    echo "Installation cancelled"
+else
+    echo "Invalid input. Please enter 'y' or 'n'."
+fi
