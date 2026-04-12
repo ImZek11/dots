f84e1d0ed41171cb8e17300a75ddcae221028ec4
Zek11 <ezequieldsromano@gmail.com>
2026-04-12 08:41:12 -0300

    Update 04-post-install.sh

--- scripts/install/04-post-install.sh
+++ scripts/install/04-post-install.sh
@@ -4,4 +4,6 @@ rm -rf go .cargo
 rm -rf ~/.config/go
 sudo rm -rf /usr/share/applications/*
 sudo cp ~/dots11/.desktop/* /usr/share/applications/
+sudo rm -rf /usr/lib/sddm/sddm.conf.d/*
+sudo cp ~/dots11/sddm/* /usr/lib/sddm/sddm.conf.d/
 echo "Post-install completed successfully."
