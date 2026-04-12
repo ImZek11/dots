0abf60615e7bd206ed00d32bf32feeef20f5a41b
Zek11 <ezequieldsromano@gmail.com>
2026-04-12 09:59:53 -0300

    Update 04-post-install.sh

--- scripts/install/04-post-install.sh
+++ scripts/install/04-post-install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-rm -rf go .cargo
+rm -rf ~/go
+rm -rf ~/.cargo
 rm -rf ~/.config/go
 sudo rm -rf /usr/share/applications/*
 sudo cp ~/dots11/.desktop/* /usr/share/applications/
 sudo rm -rf /usr/lib/sddm/sddm.conf.d/*
-sudo cp ~/dots11/sddm/* /usr/lib/sddm/sddm.conf.d/
+sudo cp ~/dots11/sddm/default.conf /usr/lib/sddm/sddm.conf.d/
 echo "Post-install completed successfully."
