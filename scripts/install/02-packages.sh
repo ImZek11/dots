4c9df72d14a75358df8afa890e621e18f6739c50
Zek11 <ezequieldsromano@gmail.com>
2026-04-11 21:50:27 -0300

    Create 02-packages.sh

---
+++ scripts/install/02-packages.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+packages=(
+  hyprland kitty fish sddm ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji
+  noto-fonts-extra elephant-bin elephant-providerlist-bin elephant-desktopapplications-bin
+  waybar fastfetch yazi swayn hyprshot hyprpaper hypridle hyprlock polkit-gnome nvim btop
+  impala wiremix cava rmpc nwg-look mpd mpc walker peaclock oh-my-posh firefox
+)
+echo "Installing packages..."
+yay -S --noconfirm "${packages[@]}"
+
+echo "Enabling services..."
+sudo systemctl enable sddm
+sudo systemctl enable mpd
+sudo systemctl start mpd
+
+echo "Programs installed, services enabled, and SDDM configured successfully!"
