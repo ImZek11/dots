1547c0cadc94b78a3a608ebab67da09b5194c2e7
Zek11 <ezequieldsromano@gmail.com>
2026-04-15 10:05:36 -0300

    Update 01-copy-configs.sh

--- scripts/01-copy-configs.sh
+++ scripts/01-copy-configs.sh
@@ -10,9 +10,11 @@ mkdir -p ~/.peaclock
 # 2. Move configs and pictures (non-overwriting)
 mv -n ~/dots11/.config/* ~/.config/ 2>/dev/null || true
 mv -n ~/dots11/Pictures/* ~/Pictures/ 2>/dev/null || true
-mv -n ~/dots11/.peaclock/* ~/peaclock/ 2>/dev/null || true
 
-# 3. Ensure the dots11 Fish config is the active one
+# 3. Copy peaclock config
+cp ~/dots11/.peaclock/config ~/.peaclock
+
+# 4. Ensure the dots11 Fish config is the active one
 cp ~/dots11/.config/fish/config.fish ~/.config/fish/config.fish
 
 echo "Configs, Pictures, Peaclock and Seamless login installation completed!"
