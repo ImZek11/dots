1a1850216c5811806840f9dfc123b52a8a43c69b
Zek11 <ezequieldsromano@gmail.com>
2026-04-17 21:23:46 -0300

    Update 05-themes.sh

--- scripts/05-themes.sh
+++ scripts/05-themes.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 themes=(catppuccin-gtk-theme-mocha catppuccin-cursors-mocha)
 yay -S --noconfirm "${themes[@]}"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-blue-standard+default"
 echo "Theme installation completed!"
