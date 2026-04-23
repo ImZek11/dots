e48cfcde7d99c5b00ad30d02efdb4b5eef1c75f4
Zek11 <ezequieldsromano@gmail.com>
2026-04-23 08:22:27 -0300

    Update 05-themes.sh

--- scripts/05-themes.sh
+++ scripts/05-themes.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
-themes=(catppuccin-gtk-theme-mocha catppuccin-cursors-mocha)
+themes=(catppuccin-gtk-theme-mocha catppuccin-cursors-mocha papirus-icon-theme)
 yay -S --noconfirm "${themes[@]}"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-blue-standard+default"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
 echo "Theme installation completed!"
