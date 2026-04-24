b6fbb5da9469bb7c9c2666cf1b6bc9f5ffb83717
Zek11 <ezequieldsromano@gmail.com>
2026-04-24 15:20:52 -0300

    Update 08-themes.sh

--- scripts/08-themes.sh
+++ scripts/08-themes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-themes=(catppuccin-gtk-theme-mocha catppuccin-cursors-mocha papirus-icon-theme kvantum-theme-catppuccin-git kvantum qt5ct qt6ct kvantum-qt5 kvantum-qt6)
+themes=(catppuccin-gtk-theme-mocha catppuccin-cursors-mocha papirus-icon-theme nwg-look kvantum-theme-catppuccin-git kvantum qt5ct qt6ct kvantum-qt5 kvantum-qt6)
 yay -S --noconfirm "${themes[@]}"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-blue-standard+default"
